54f29c03f0cb792e22c2509a6940a3cac40bc4cc
lkhagva99 <b.lkhagva1101@gmail.com>
2024-11-04 08:29:56 +0000

    config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,4 +14,7 @@ export default [
       "no-undef": "error",
     },
   },
+  {
+    ignores: [".next/*"]
+}
 ];
--- next.config.js
+++ next.config.js
@@ -5,15 +5,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    domains: [
-      "source.unsplash.com",
-      "m.media-amazon.com",
-      "127.0.0.1",
-      "api.urga.mn",
-      "lh3.googleusercontent.com",
-      "platform-lookaside.fbsbx.com",
-      "test.test.com",
-    ],
     remotePatterns: [
       {
         protocol: "https",
