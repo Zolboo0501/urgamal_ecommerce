9b61fa73ed47e8d3c0985e5fcfde76b086a9e1fd
Odgerel Zolboo <zolboo@Odgerels-MacBook-Pro.local>
2024-12-02 02:02:30 +0800

    fix products page

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,9 +1,6 @@
 /* eslint-disable no-undef */
-/* eslint-disable react/prop-types */
 import ProductGridList from "@/components/ProductGridList/ProductGridList";
 import { PAGE_SIZE } from "@/utils/constant";
-import { fetchMethod } from "@/utils/fetch";
-import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
 import CategoryLayout from "@/components/GlobalLayout/CategoryLayout";
 import ProductCard from "@/components/ProductCard";
@@ -11,92 +8,80 @@ import { Pagination } from "@mantine/core";
 import axios from "axios";
 import useSWRInfinite from "swr/infinite";
 
-export async function getServerSideProps() {
-  const data = await fetchMethod("GET", "product");
-  return {
-    props: {
-      initialData: data,
-    },
-  };
-}
-
-export default function SearchResult({ initialData }) {
-  const router = useRouter();
-  const { q } = router.query;
+export default function SearchResult() {
+  const [loading, setLoading] = useState(false);
   const [products, setProducts] = useState([]);
-  const [total, setTotal] = useState(0);
   const [activePage, setActivePage] = useState(1);
-  const fetcher = async (url) =>
-    axios
-      .get(url, { headers: { "Content-Type": "application/json" } })
-      .then((res) => {
-        return { data: res?.data?.result, total: res.data?.meta?.total };
-      })
-      .catch((error) => console.log(error, "err in fetcher"));
 
-  const { data, setSize, isLoading } = useSWRInfinite(
-    (index) =>
-      `${process.env.NEXT_PUBLIC_API_URL}/product?offset=${
-        (index + 1) * 20
-      }&limit=${PAGE_SIZE}`,
-    fetcher,
-    { revalidateFirstPage: false },
-  );
+  const fetcher = async (url) => {
+    try {
+      const res = await axios.get(url, {
+        headers: { "Content-Type": "application/json" },
+      });
+      return { data: res?.data?.result, total: res?.data?.meta?.total };
+    } catch (error) {
+      console.log("Error in fetcher", error);
+    }
+  };
 
-  const isEmpty = products?.[0]?.length === 0;
+  const getKey = (pageIndex, previousPageData) => {
+    if (previousPageData && previousPageData.data?.length === 0) return null; // End of pagination
+    return `${process.env.NEXT_PUBLIC_API_URL}/product?offset=${
+      pageIndex * PAGE_SIZE
+    }&limit=${PAGE_SIZE}`;
+  };
+
+  const { data, setSize } = useSWRInfinite(getKey, fetcher, {
+    revalidateFirstPage: false,
+  });
 
   useEffect(() => {
-    if (data?.length > 0) {
+    if (data?.length > 0 && data?.[0]?.data) {
+      setLoading(true);
       setProducts(data?.[data?.length - 1]?.data);
-      setTotal(data?.[data?.length - 1]?.total);
-      // setProducts(data?.[data?.length - 1]);
-    } else {
-      setProducts(initialData?.result);
-      setTotal(initialData?.meta?.total);
+      setLoading(false);
     }
   }, [data]);
 
-  const fetchMore = (value) => {
-    setSize(value);
+  const initPage = () => {
+    setSize(1);
+    setActivePage(1);
   };
 
-  useEffect(() => {
-    setProducts(initialData?.result);
-    setTotal(initialData?.pagination?.total);
-  }, []);
+  const fetchMore = (page) => {
+    setActivePage(page);
+    setSize(page);
+  };
 
   return (
-    <CategoryLayout>
+    <CategoryLayout initPage={initPage}>
       <div className="flex min-h-screen w-full gap-6 px-10 py-12">
         <div className="flex flex-col">
           <ProductGridList
-            showSkeleton={isLoading}
-            isEmpty={isEmpty}
-            emptyStateMessage="хайлтад тохирох бараа олдсонгүй"
-            query={q}
+            showSkeleton={loading}
+            emptyStateMessage="Ангиллын бараа олдсонгүй"
+            isEmpty={!products || products?.length === 0}
           >
-            {products?.map((e, index) => {
-              return (
+            {products?.length > 0 &&
+              products.map((e, index) => (
                 <ProductCard
                   key={`product-card-key-${index}-${e?.id}`}
                   src={e?.additionalImage?.[0]}
                   additionalImages={e.additionalImage?.slice(1)}
                   data={e}
                 />
-              );
-            })}
+              ))}
           </ProductGridList>
           <div className="mt-8 flex items-center justify-center">
             <Pagination
-              total={total / 20 + 1}
+              total={
+                data?.[0]?.total ? Math.ceil(data[0].total / PAGE_SIZE) : 0
+              }
               color="teal"
               radius="xl"
               value={activePage}
-              onChange={(value) => {
-                setActivePage(value);
-                fetchMore(value - 1);
-              }}
               siblings={2}
+              onChange={(value) => fetchMore(value)}
             />
           </div>
         </div>
